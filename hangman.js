b4e623f313345fdad105d07b26b3a5ce6ba7de76
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-10 15:49:54 +0200

    Update hangman.js
    Add the solution and test cases

--- hangman.js
+++ hangman.js
@@ -1,1 +1,32 @@
 //Given is a string hangmanString. Replace every second letter with an underscore and return the result as a string. The first character must be an underscore.
+
+function hangman(hangmanString) {
+    let hangStr = '';
+    for (let i = 0; i < hangmanString.length; i += 1) {
+        if (i % 2 == 0) {
+            hangStr += '_';
+        } else {
+              hangStr += hangmanString[i];
+        }
+    }
+    return hangStr;
+}
+
+/*
+Test Cases:
+
+hangman('TestString')
+'_e_t_t_i_g'
+
+hangman('Foobar')
+'_o_b_r'
+
+hangman('Demo')
+'_e_o'
+
+hangman('JS is fancy')
+'_S_i_ _a_c_'
+
+hangman('ThisIsAnFancyString')
+'_h_s_s_n_a_c_S_r_n_'
+*/
