19ee14e769c37d4c40b223c4b37e0154a81cc846
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-10 15:50:56 +0200

    Create camelCase.js

---
+++ camelCase.js
@@ -0,0 +1,1 @@
+// Given is a string n. The task is to return the sentence in CamelCase notation. This means that each new word is capitalized and immediately appended to the old word. Start with a lowercase letter.
