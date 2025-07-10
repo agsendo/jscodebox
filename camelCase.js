69c7b678fac08fdcce301a6215daeed3edd4b79f
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-10 15:52:15 +0200

    Update camelCase.js

--- camelCase.js
+++ camelCase.js
@@ -1,1 +1,25 @@
-// Given is a string n. The task is to return the sentence in CamelCase notation. This means that each new word is capitalized and immediately appended to the old word. Start with a lowercase letter.
+/*
+Given is a string n. The task is to return the sentence in CamelCase notation.
+This means that each new word is capitalized and immediately appended to the old word.
+Start with a lowercase letter.
+*/
+
+function camelCase(n) {
+    let arrWords = n.split(' ');
+    let camel = '';
+    let tempString = '';
+  
+    for (let i in arrWords) {
+      if(arrWords[i]) {
+         tempString = arrWords[i][0].toUpperCase() + arrWords[i].slice(1);
+        //console.log(tempString);
+        camel += tempString;
+        //console.log(camel);
+      }
+    }
+    if (camel.length > 0) {
+       camel = camel[0].toLowerCase() + camel.slice(1);
+    } 
+    //console.log(camel);
+    return camel;
+}
