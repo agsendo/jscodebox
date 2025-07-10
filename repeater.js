788675dad61ebb89298e02a8c76b6e67fd81db50
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-10 15:55:13 +0200

    Update repeater.js

--- repeater.js
+++ repeater.js
@@ -1,1 +1,35 @@
 //Given is a String string and a number n. Return a string with the section from 0 to n in a row. In each run n is to be decremented.
+
+function repeater(string, n) {
+    let newString = '';
+    while (n > 0) {
+        for (let i = 0; i < n; i += 1) {
+            if (i < string.length) {
+                newString += string[i];
+            } else {
+                newString += '';
+            }
+        }
+        n -= 1;
+    }
+    return newString;
+}
+
+/*
+Test Cases:
+
+repeater('JSCodebox',6)
+'JSCodeJSCodJSCoJSCJSJ'
+
+repeater('Foobar',2)
+'FoF'
+
+repeater('',2)
+''
+
+repeater('Hello coder!',5)
+'HelloHellHelHeH'
+
+repeater('Hi',3)
+'HiHiH'
+*/
