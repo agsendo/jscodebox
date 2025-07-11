5c4a9d769485087a1d0f5f40accef6ae2b7d5562
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-11 13:53:00 +0200

    Update dom1.js

--- dom1.js
+++ dom1.js
@@ -1,11 +1,50 @@
 /*
 TASKS:
 1 - Add a h1 to the body using a function that takes 'headline' text as argument
+2 - Change the background color of .bg element to blue
+3 - Set the checkbox with given id to checked
+4 - Read the first nad last name from the inputs (#firstname and #lastname) and write the full name into the #fullname input. If an input is empty, use '#' as its value.
 */
 
 
+// -- 1 --
 function addHeadline(headline) {
     const h1 = document.createElement("h1");
     h1.textContent = headline;
     document.body.appendChild(h1);
+}
+
+
+// -- 2 --
+let bgElements = document.getElementsByClassName("bg");
+bgElements[0].style.backgroundColor = "blue";
+
+
+// -- 3 --
+function checkMe() {
+    document.getElementById("my-checkbox").checked = true;
+}
+
+
+// -- 4 --
+function whatIsMyName() {
+    const firstNameInput = document.getElementById("firstname");
+    const lastNameInput = document.getElementById("lastname");
+    const fullNameInput = document.getElementById("fullname");
+
+    let firstName;
+    if (firstNameInput.value.trim() !== '') {
+        firstName = firstNameInput.value;
+    } else {
+        firstName = '#';
+    }
+
+    let lastName;
+    if (lastNameInput.value.trim() !== '') {
+        lastName = lastNameInput.value;
+    } else {
+        lastName = '#';
+    }
+
+    fullNameInput.value = firstName + ' ' + lastName;
 }
