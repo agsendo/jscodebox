bc5eaec3ef26f30ba6f0ac9fe0dd0cc39b8f2ced
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-11 13:54:29 +0200

    Create cinemaQueue.js

---
+++ cinemaQueue.js
@@ -0,0 +1,6 @@
+/*
+TASK: Given is a number max representing the maximum number of groups in the queue. You also have an array visitors of people,
+where 'X' stands for one person and 'O' for one companion. A group is minimum one 'O' and any number of 'X'.
+Check if all groups fit into the queue. Return 'full' if all fit. Otherwise, return the number of groups that are too many or not full.
+*/
+
