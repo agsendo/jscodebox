56eed6c935718095ede24ceb5cc0cd51a684e1d1
agsendo <129989184+agsendo@users.noreply.github.com>
2025-07-13 13:58:37 +0200

    Update gHappy.js

--- gHappy.js
+++ gHappy.js
@@ -1,6 +1,10 @@
 // TASK: A small 'g' is 'happy', but only if a small 'g' follows before or after it. Return true if all g's are happy.
 
-// SOLUTION:
+/*
+SOLUTION: Iterate through string. If there is a 'g', check for the neighbouring letters.
+If the index i is bigger than 0 (previous character exists) or the index i is smaller than length of string -1
+(there is still at least 1 character), check a character before and after. If it is another 'g', it is 'happy'.
+*/
 
 
 function gHappy(string) {
@@ -18,3 +22,46 @@ function gHappy(string) {
     }
     return ok;
 }
+
+
+/*
+TEST CASES:
+gHappy('xyggxyz')
+true
+
+gHappy('Huggy Wuggy')
+true
+
+gHappy('xxzzgxxzz')
+false
+
+gHappy('garage of eggs')
+false
+
+gHappy('yyzzgxxzzggzzy')
+false
+
+gHappy('Not bigGgame!')
+false
+
+gHappy('')
+false
+
+gHappy('g')
+false
+
+gHappy('gg')
+true
+
+gHappy('egg')
+true
+
+gHappy('BIGger!')
+false
+
+gHappy('The bigggest egggame!')
+true
+
+gHappy('garage')
+false
+*/
